5d1dae14a0ef6cabee8b754de4b6ac7bc31206e4
Peter Berry <pwcberry@gmail.com>
2015-01-19 20:41:47 +1100

    Added gulp tasks "make-pages" and "save-pages"

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('serve', function () {
         }));
 });
 
-gulp.task('make', function () {
+gulp.task('make-pages', function () {
     shellActions([
         ['git branch -D gh-pages', 'Removing branch "gh-pages"...'],
         ['git branch gh-pages', 'Adding branch "gh-pages"...'],
@@ -22,8 +22,20 @@ gulp.task('make', function () {
         ['cp -r dist/* .', 'Copying application files into place...'],
         ['rm -rf dist', 'Removing "dist" folder...'],
         ['git add --all .', 'Adding changes to repo...'],
-        ['git commit -m "Pages Made"', 'Committing to repo...']
+        ['git commit -m "Pages Made"', 'Committing to repo...'],
+        ['git checkout master', 'Returning to "master" branch...']
     ]);
 });
+
+gulp.task('save-pages', function() {
+    shellActions([
+        ['git checkout gh-pages', 'Checking out "gh-pages"...'],
+        ['git fetch origin gh-pages', 'Fetching "gh-pages" from remote...'],
+        ['git merge -s ours origin/gh-pages', 'Merging our changes...'],
+        ['git push origin gh-pages', 'Saving "gh-pages" to remote...'],
+        ['git checkout master', 'Returning to "master" branch...']
+    ])
+});
+
 
 gulp.task('default', ['serve']);
